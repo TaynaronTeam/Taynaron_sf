7aabf36dd55065226b5ace27f97b07cd5af29553
XaiMeS <sewikrk@gmail.com>
2018-11-04 19:57:57 +0100

    Now its good...

--- src/mainline_released/mainline_sg/Srcs/Server/game/src/input_main.cpp
+++ src/mainline_released/mainline_sg/Srcs/Server/game/src/input_main.cpp
@@ -694,13 +694,8 @@ int CInputMain::Chat(LPCHARACTER ch, const char * data, size_t uiBytes)
 	}
 
 	char chatbuf[CHAT_MAX_LEN + 1];
-	int len;
-	
-	#ifdef ENABLE_GLOBAL_CHAT
-		len = snprintf(chatbuf, sizeof(chatbuf), "[CH%d][|cFF00FF00Lv. %d|r] %s : %s", g_bChannel, ch->GetLevel(), ch->GetName(), buf);
-	#else
-		len = snprintf(chatbuf, sizeof(chatbuf), "%s : %s", ch->GetName(), buf);
-	#endif
+	int len = snprintf(chatbuf, sizeof(chatbuf), "%s : %s", ch->GetName(), buf);
+
 	if (CHAT_TYPE_SHOUT == pinfo->type)
 	{
 		LogManager::instance().ShoutLog(g_bChannel, ch->GetEmpire(), chatbuf);
@@ -746,35 +741,18 @@ int CInputMain::Chat(LPCHARACTER ch, const char * data, size_t uiBytes)
 
 		ch->SetLastShoutPulse(thecore_heart->pulse);
 
-		char chatbuf_global[CHAT_MAX_LEN + 1];
+		TPacketGGShout p;
 
-		#ifdef ENABLE_GLOBAL_CHAT
-				
-				const char* kingdoms[3] = {"1     ","2     ","3     "};
-				
-				if(ch->GetEmpire() > 0 && ch->GetEmpire() == 1 or ch->GetEmpire() == 2 or ch->GetEmpire() == 3 ) 
-				{
-					int len_global = snprintf(chatbuf_global, sizeof(chatbuf_global), "%s [CH%d][|cFF00FF00Lv. %d|r] %s : %s",kingdoms[ch->GetEmpire()-1], g_bChannel, ch->GetLevel(), ch->GetName(), buf);
-				}
-				else 
-				{
-					int len_global = snprintf(chatbuf_global, sizeof(chatbuf_global), "%s : %s",ch->GetName(), buf);
-				}
+		p.bHeader = HEADER_GG_SHOUT;
+		p.bEmpire = ch->GetEmpire();
+		strlcpy(p.szText, chatbuf, sizeof(p.szText));
 
-		#else
+		P2P_MANAGER::instance().Send(&p, sizeof(TPacketGGShout));
 
-				TPacketGGShout p;
-		
-				p.bHeader = HEADER_GG_SHOUT;
-				p.bEmpire = ch->GetEmpire();
-				strlcpy(p.szText, chatbuf_global, sizeof(p.szText));
-		
-				P2P_MANAGER::instance().Send(&p, sizeof(TPacketGGShout));
-		
-				SendShout(chatbuf_global, ch->GetEmpire());
+		SendShout(chatbuf, ch->GetEmpire());
 
-				return (iExtraLen);
-			}
+		return (iExtraLen);
+	}
 
 	TPacketGCChat pack_chat;
 
